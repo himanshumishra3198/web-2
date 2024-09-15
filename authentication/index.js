ced215dbe6c888ef1e3d5b050ff4cb81343fd950
Himanshu Mishra <mickeysingh.r4@gmail.com>
2024-09-15 13:43:11 +0530

    Add authentication app

---
+++ authentication/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+app.use(express.json());
+
+let users = [];
+const JWT_SECRET = "JackDaniels";
+
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  users.push({
+    username,
+    password,
+  });
+  res.status(201).send("User signed successfully!");
+});
+
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  let user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (user) {
+    let token = jwt.sign(username, JWT_SECRET);
+    user.token = token;
+    res.status(200).send({
+      token,
+    });
+  } else {
+    res.status(401).send({
+      message: "Incorrect username or password",
+    });
+  }
+});
+
+app.get("/me", (req, res) => {
+  const token = req.headers.authorization;
+
+  const username = jwt.verify(token, JWT_SECRET);
+
+  let user = users.find((user) => user.username === username);
+  if (user) {
+    res.status(200).send(user);
+  } else {
+    res.status(401).send({
+      message: "Please provide a valid token",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("App is listening on port 3000");
+});
