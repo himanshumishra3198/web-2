2441cf60ec087c86391c50dac4f36a99ca36db5a
Himanshu Mishra <mickeysingh.r4@gmail.com>
2024-09-23 23:21:37 +0530

    Implemented todo routes

--- todo-app/backend/index.js
+++ todo-app/backend/index.js
@@ -5,6 +5,8 @@ const { userModel, todoModel } = require("./db");
 
 const app = express();
 const PORT = 3000;
+const JWT_SECRET = "Jai Bajrang Bali";
+
 app.use(express.json());
 
 mongoose.connect(
@@ -32,22 +34,84 @@ app.post("/signup", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  const username = req.body.username;
+  const email = req.body.email;
   const password = req.body.password;
 
   const user = await userModel.findOne({
-    username: username,
+    email: email,
     password: password,
   });
 
   if (user) {
-    const token = jwt.sign(user._id, JWT_SECRET);
+    const token = await jwt.sign({ userId: user._id }, JWT_SECRET);
     res.status(200).json({
       token: token,
     });
   } else {
     res.status(401).json({
       message: "email or password is incorrect",
+    });
+  }
+});
+
+function auth(req, res, next) {
+  const token = req.headers.authorization;
+  try {
+    let payload = jwt.verify(token, JWT_SECRET);
+    req.body.userId = payload.userId;
+    next();
+  } catch (e) {
+    res.status(401).json({
+      message: "You are unauthorized",
+    });
+  }
+}
+// auth middleware except signup and signin
+app.use(auth);
+
+app.post("/todo", async (req, res) => {
+  try {
+    await todoModel.create({
+      title: req.body.title,
+      description: req.body.description,
+      userId: req.body.userId,
+    });
+    res.status(201).json({
+      message: "Todo saved Successfully",
+    });
+  } catch (e) {
+    res.status(403).json({
+      message: "Internal Server error",
+    });
+  }
+});
+
+app.put("/todo", async (req, res) => {
+  try {
+    let todo = await todoModel.findById({ _id: req.body.todoId });
+    todo.title = req.body.title;
+    todo.description = req.body.description;
+    await todo.save();
+    res.status(200).json({
+      message: "todo updated successfully",
+      todo,
+    });
+  } catch (e) {
+    res.status(401).json({
+      message: e,
+    });
+  }
+});
+
+app.get("/todos", async (req, res) => {
+  try {
+    let todos = await todoModel.find({
+      userId: req.body.userId,
+    });
+    res.status(200).json(todos);
+  } catch (e) {
+    res.status(403).json({
+      message: e,
     });
   }
 });
