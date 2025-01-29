4df174d8f03475355740cb2574c64beafa84c9ab
Turbobot <turbobot@vercel.com>
2025-01-29 20:37:15 +0530

    feat(create-turbo): apply official-starter transform

--- chalk-room/apps/ws-server/src/index.js
+++
@@ -1,8 +0,0 @@
-import { WebSocketServer } from "ws";
-
-const wss = new WebSocketServer({ port: 8080 });
-
-wss.on("connection", (socket) => {
-  console.log("a new user joined the room");
-  socket.send("welcome!");
-});
